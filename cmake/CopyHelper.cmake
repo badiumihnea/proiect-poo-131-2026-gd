4e367fa7c8d14887d6870004ec96295ebc38c24c
badiumihnea <badiumihnea16@gmail.com>
2026-05-23 20:27:03 +0300

    Fixed bugs related to SFML setup

--- cmake/CopyHelper.cmake
+++ cmake/CopyHelper.cmake
@@ -8,21 +8,21 @@ function(copy_files)
     # copy files to build dir
     foreach(file ${ARG_FILES})
         add_custom_command(
-            TARGET ${ARG_TARGET_NAME} POST_BUILD
-            COMMENT "Copying ${file}..."
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            ${CMAKE_SOURCE_DIR}/${file} $<TARGET_FILE_DIR:${ARG_TARGET_NAME}>)
-            # ${CMAKE_CURRENT_BINARY_DIR})
+                TARGET ${ARG_TARGET_NAME} POST_BUILD
+                COMMENT "Copying ${file}..."
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                ${CMAKE_SOURCE_DIR}/${file} $<TARGET_FILE_DIR:${ARG_TARGET_NAME}>)
+        # ${CMAKE_CURRENT_BINARY_DIR})
     endforeach()
 
     # copy folders to build dir
     foreach(dir ${ARG_DIRECTORY})
         add_custom_command(
-            TARGET ${ARG_TARGET_NAME} POST_BUILD
-            COMMENT "Copying directory ${dir}..."
-            COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different
-            ${CMAKE_SOURCE_DIR}/${dir} $<TARGET_FILE_DIR:${ARG_TARGET_NAME}>/${dir})
-            # ${CMAKE_CURRENT_BINARY_DIR}/${dir})
+                TARGET ${ARG_TARGET_NAME} POST_BUILD
+                COMMENT "Copying directory ${dir}..."
+                COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different
+                ${CMAKE_SOURCE_DIR}/${dir} $<TARGET_FILE_DIR:${ARG_TARGET_NAME}>/${dir})
+        # ${CMAKE_CURRENT_BINARY_DIR}/${dir})
     endforeach()
 
     if(ARG_COPY_TO_DESTINATION)
