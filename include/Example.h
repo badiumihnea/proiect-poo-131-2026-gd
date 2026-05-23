3830194e916294cb3fde30cc211e2e5c2a095c58
badiumihnea <badiumihnea16@gmail.com>
2026-05-23 22:23:01 +0300

    Fixed bugs related to ticks (commented Example header and source file in include and src respectively)

--- include/Example.h
+++ include/Example.h
@@ -1,12 +1,14 @@
-#ifndef EXAMPLE_H
-#define EXAMPLE_H
+//Ignore
 
-class Example {
-    int x = 1;
-    int y = 2;
-    void f() const;
-public:
-    void g();
-};
-
-#endif // EXAMPLE_H
+// #ifndef EXAMPLE_H
+// #define EXAMPLE_H
+//
+// class Example {
+//     int x = 1;
+//     int y = 2;
+//     void f() const;
+// public:
+//     void g();
+// };
+//
+// #endif // EXAMPLE_H
